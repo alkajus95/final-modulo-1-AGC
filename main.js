4419098f0692ec4cd7e94a610fb1c347b9679daf
alkajus95 <alejograciacarrillo@gmail.com>
2025-06-04 21:38:33 -0500

    ¨fin¨

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-function modoOscuro(){
+function modoOscuro() {
     document.body.classList.toggle("dark-mode");
-    document.getElementById("dark-mode-toogle").addEventListener("click");
+    const btn = document.getElementById("dark-mode-toggle");
+    if (document.body.classList.contains("dark-mode")) {
+        btn.textContent = "Modo Claro";
+    } else {
+        btn.textContent = "Modo Oscuro";
     }
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("dark-mode-toggle");
+    if (document.body.classList.contains("dark-mode")) {
+        btn.textContent = "Modo Claro";
+    } else {
+        btn.textContent = "Modo Oscuro";
+    }
+});
